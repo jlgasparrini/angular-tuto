1e27cbe4bfb7d977abc24bb34b0a52a32d293712
Jonathan Leonel Gasparrini <jlgasparrini@gmail.com>
2016-03-03 14:46:25 -0300

    Add more menus on PizzaPlanet

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -18,4 +18,40 @@ app.controller('MainController', ['$scope', function($scope) {
       price: 4.95
     }
   ];
+
+  $scope.mains = [
+    {
+      name: 'Caprese',
+      description: 'Mozzarella, tomatoes, basil, balsmaic glaze.',
+      price: 4.95
+    },
+    {
+      name: 'Mozzarella Sticks',
+      description: 'Served with marinara sauce.',
+      price: 3.95
+    },
+    {
+      name: 'Bruschetta',
+      description: 'Grilled bread garlic, tomatoes, olive oil.',
+      price: 4.95
+    }
+  ];
+
+  $scope.extras = [
+    {
+      name: 'Caprese',
+      description: 'Mozzarella, tomatoes, basil, balsmaic glaze.',
+      price: 4.95
+    },
+    {
+      name: 'Mozzarella Sticks',
+      description: 'Served with marinara sauce.',
+      price: 3.95
+    },
+    {
+      name: 'Bruschetta',
+      description: 'Grilled bread garlic, tomatoes, olive oil.',
+      price: 4.95
+    }
+  ];
 }]);
